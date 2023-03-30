94bedb911be2d882323cc6f5d5a645e514ff09e4
The-ARJ <joshiaayush871@gmail.com>
2023-03-30 15:03:43 +0545

    improved

--- src/utils/Service.js
+++ src/utils/Service.js
@@ -4,6 +4,7 @@ export const resultURL = "/marks/my-marks";
 
 const result = (notifyDetails) => {
   return axios.get(`${resultURL}`, {
+    withCredentials: true,
     headers: {
       Authorization: `bearer ${window.localStorage.getItem("token")}`,
       "Content-Type":"application/x-www-form-urlencoded"
